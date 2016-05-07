78390fb7c8cc276ace3512f2e38c8f1db9a1aeeb
Brian Holland <beehollander@gmail.com>
2016-05-06 23:49:46 -0500

    Done.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
 // code solution here
+
+function President (name, politicalParty, yearsInOffice, homeState){
+  this.name = name;
+  this.politicalParty = politicalParty;
+  this.yearsInOffice = yearsInOffice;
+  this.homeState = homeState;
+
+  President.prototype.veto = function(){
+    return "NO!";
+  };
+
+  President.prototype.passBill = function(){
+    return "You can do that!";
+  };
+
+  President.prototype.doCharity = function(){
+    return "I like to help people.";
+  };
+
+  President.prototype.conductPressInterview = function(){
+    return "I am proud to be an American.";
+  };
+
+  President.prototype.sayHi = function(){
+    var out = "Hi, my name is " + this.name;
+    out += ". I am from " + this.homeState;
+    out += ". I represent the " + this.politicalParty;
+    out += "s, and was in office " + this.yearsInOffice + ".";
+    return out;
+  };
+};
